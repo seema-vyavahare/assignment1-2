75d5c7490c005e4c3de480703858b8b5ec2ddd8b
Seema <vyavahareseema245@gmail.com>
2025-12-18 07:27:22 +0530

    addede

--- Assignment3/Array/SecondLarge.java
+++ Assignment3/Array/SecondLarge.java
@@ -1,6 +1,6 @@
 class Demo{
     public static void main(String[]args){
-        int[]arr={5,1,3};
+        int[]arr={10,20,30,5};
         int max1=arr[0];
         int max2=arr[0];
         for(int i=1;i<arr.length;i++){
